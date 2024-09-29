481f41d360d7b414b1c66f199ac08aaf4dfe4a59
Sky Randy <soras1758@gmail.com>
2024-09-29 12:33:12 +0900

    buildTree method

--- bst.js
+++ bst.js
@@ -7,5 +7,26 @@ class Node{
 }
 
 class Tree{
+    constructor(arr){
+        this.arr=arr;
+        this.root=null;
+    }
 
+    buildTree(array){
+        function sortArr(array){
+            return array.filter((item,
+                index)=>array.indexOf(item)===index);
+        }
+        let sortedArr=sortArr(array).sort((a,b)=>a-b);
+
+        function sortedArrayToBST(sortedArr,start,end){
+            if(start>end) return null;
+            let mid = parseInt((start + end)/2);
+            let node =new Node(sortedArr[mid]);
+            sortedArr.left=sortedArrayToBST(sortedArr,start,mid -1);
+            sortedArr.right=sortedArrayToBST(sortedArr,mid + 1,end);
+
+            return node;
+        }
+    }
 }
